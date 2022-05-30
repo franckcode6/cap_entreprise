6e0f6b2f203eefce201ca36e86988091593c7808
Franck Quasevi <franckcode6@gmail.com>
2022-05-30 14:31:40 +0200

    Creation 1er controller

--- src/main/java/fr/humanbooster/cap_entreprise/controller/IndexController.java
+++ src/main/java/fr/humanbooster/cap_entreprise/controller/IndexController.java
@@ -1,6 +1,8 @@
 package fr.humanbooster.cap_entreprise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.AllArgsConstructor;
 
@@ -8,4 +10,12 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class IndexController {
 
+	@GetMapping({ "/index", "/" })
+	public ModelAndView indexGet() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("index");
+		
+		return mav;
+	}
 }
